c6687098b016cc21ca86f09e41408db70cf45b33
MITSUBOSHI <mitsuboshi.yuya@moneyforward.co.jp>
2020-03-13 21:44:13 +0900

    add setting: goenv

--- extend.sh
+++ extend.sh
@@ -13,6 +13,16 @@ elif [[ -s $HOME/.rvm/scripts/rvm ]] ; then
 fi
 
 # ------------------------------
+# goenv
+# ------------------------------
+if [[ -s ~/.goenv  ]] ; then
+  export GOENV_ROOT="$HOME/.goenv"
+  export PATH="$GOENV_ROOT/bin:$PATH"
+  export GOENV_DISABLE_GOPATH=1
+  eval "$(goenv init -)"
+fi
+
+# ------------------------------
 # phpenv
 # ------------------------------
 if [[ -x "`which phpenv`" ]] ; then
