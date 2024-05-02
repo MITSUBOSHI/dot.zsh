183f78519b7af6e17e92390265e93ee9416a61e4
mitsubosh <yuya.mitsuboshi@gmail.com>
2024-05-02 11:33:35 +0900

    remove ANDROID envs

--- extend.sh
+++ extend.sh
@@ -75,14 +75,6 @@ export PGDATA=/usr/local/var/postgres
 export SBT_OPTS='-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=1024M'
 
 # ------------------------------
-# Android
-# ------------------------------
-export ANDROID_HOME=/Applications/android-sdk-macosx
-export PATH=$PATH:$ANDROID_HOME/tools
-export PATH=$PATH:$ANDROID_HOME/platform-tools
-export PATH=$PATH:$ANDROID_HOME/build-tools
-
-# ------------------------------
 # Haskell
 # ------------------------------
 export PATH=$PATH:$HOME/.cabal/bin
